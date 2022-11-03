584bb7c647734777a6aff269f49e076b3b575519
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-03 14:01:22 -0300

    utiliza o create_list com a factory

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Order, type: :model do
 
     expect(order.customer).to be_kind_of(Customer) 
   end
+  it 'Tem 3 pedidos' do 
+    orders = create_list(:order, 3) #cria 3 pedidos com o list e o parametro 
+
+    expect(orders.count).to eq(3) 
+  end
 end

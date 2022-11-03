5cc72439160b85bf212d2798298fa604bdb7b537
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-03 13:57:01 -0300

    teste de order com a factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :order do
     sequence(:description) {|n| "Pedido número - #{n}"}
-    customer 
+    customer #já faz a ligação com o customer devido a FK
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Tem 1 pedido' do
+    order = create(:order)
+
+    expect(order.customer).to be_kind_of(Customer) 
+  end
 end

ee227b361de51d613d112f03daa38838877a708d
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-04 13:44:47 -0300

    gem httparty para requisições http

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,15 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'httparty'
+
 RSpec.configure do |config|
 
   #FactoryBot Lint: exibe onde está quebrando na factory
-  config.before(:suite) do
-    FactoryBot.lint
-  end
+  # config.before(:suite) do
+  #   FactoryBot.lint
+  # end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

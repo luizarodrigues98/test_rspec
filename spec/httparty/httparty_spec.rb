ecfd45803148d3d2526e486cbe487a494e8a5bd0
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-04 14:21:23 -0300

    gem stub_request

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,11 @@
 describe "HTTParty" do
-  it 'HTTParty' do
+  it 'content-type' do
+    stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2').
+    to_return(status: 200, body:"", headers: {'content-type': 'application/json'})
+    # stub desabilita a conexão com a internet nos testes, então precisa dessa linha acima pra criar o stub
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type'] 
-    
     expect(content_type).to match(/application\/json/) 
   end
 end
